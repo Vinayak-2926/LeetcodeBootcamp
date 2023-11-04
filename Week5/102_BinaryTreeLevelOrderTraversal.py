b97adfc942afa1dc0a3c1459c1291191381d7413
Vinayak-2926 <vs2926@nyu.edu>
2023-11-04 18:25:43 -0400

    Add week5 solutions

---
+++ Week5/102_BinaryTreeLevelOrderTraversal.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if root is None:
+            return []
+        q = [[root]]
+        for level in q:
+            record = []
+            for node in level:
+                if node.left:
+                    record.append(node.left)
+                if node.right:
+                    record.append(node.right)
+            if record:
+                q.append(record)
+        return [[x.val for x in level] for level in q]
+        
